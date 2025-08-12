f2f7fbda46b18d0fa44f32d3038e0d324cf0edff
Jhone-cmd <jhone.1941@gmail.com>
2025-08-12 10:38:27 -0300

    feat: the first development of the project was carried out

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hello, World!');
+import { createHash } from 'node:crypto'
+
+const prefix = process.argv[2]
+const input = process.argv.slice(3).join('')
+
+let nonce = 0
+let hash = ''
+
+do {
+  nonce++
+  hash = createHash('sha256').update(`${input}${nonce}`).digest('hex')
+  console.log(`input: ${input} / nonce: ${nonce} / hash: ${hash}`)
+} while (!hash.startsWith(prefix))
