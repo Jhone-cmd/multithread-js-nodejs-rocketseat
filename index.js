fe68c8acd9b60c29f0c3c627df61271917b9c7da
Jhone-cmd <jhone.1941@gmail.com>
2025-08-13 09:23:38 -0300

    feat: leaving the project as multithreaded

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 import { createHash } from 'node:crypto'
+import { isMainThread, workerData } from 'node:worker_threads'
 
-const prefix = process.argv[2]
-const input = process.argv.slice(3).join('')
+if (isMainThread) {
+  const threads = process.argv[2]
+  const prefix = process.argv[3]
+  const input = process.argv.slice(4).join('')
 
-let nonce
-let hash
+  console.log(` threads: ${threads} / prefix: ${prefix} / input: ${input}`)
 
-do {
-  nonce = `${Math.random().toFixed(18).substring(2)}${Math.random().toFixed(18).substring(2)}`
-  hash = createHash('sha256').update(`${input}${nonce}`).digest('hex')
-  console.log(`input: ${input} / nonce: ${nonce} / hash: ${hash}`)
-} while (!hash.startsWith(prefix))
+  for (let i = 0; i < threads; i++) {
+    new Worker(__filename, { workerdata: { prefix, input } })
+  }
+} else {
+  const { prefix, input } = workerData
+
+  let nonce
+  let hash
+
+  do {
+    nonce = `${Math.random().toFixed(18).substring(2)}${Math.random().toFixed(18).substring(2)}`
+    hash = createHash('sha256').update(`${input}${nonce}`).digest('hex')
+    console.log(`input: ${input} / nonce: ${nonce} / hash: ${hash}`)
+  } while (!hash.startsWith(prefix))
+}
