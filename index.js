b3dc357bb5d7ac0fc6a5906c1332a9e2ffb9dbc1
Jhone-cmd <jhone.1941@gmail.com>
2025-08-13 09:14:31 -0300

    feat: set the nonce as random

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ import { createHash } from 'node:crypto'
 const prefix = process.argv[2]
 const input = process.argv.slice(3).join('')
 
-let nonce = 0
-let hash = ''
+let nonce
+let hash
 
 do {
-  nonce++
+  nonce = `${Math.random().toFixed(18).substring(2)}${Math.random().toFixed(18).substring(2)}`
   hash = createHash('sha256').update(`${input}${nonce}`).digest('hex')
   console.log(`input: ${input} / nonce: ${nonce} / hash: ${hash}`)
 } while (!hash.startsWith(prefix))
