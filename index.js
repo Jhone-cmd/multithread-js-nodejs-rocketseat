8c562aedec414eeeaa67f417ac84c7312507b313
Jhone-cmd <jhone.1941@gmail.com>
2025-08-13 10:47:02 -0300

    feat: add the number of cpus in the thread, and the project is finished

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { createHash } from 'node:crypto'
+import { cpus } from 'node:os'
 import {
   isMainThread,
   parentPort,
@@ -7,19 +8,21 @@ import {
 } from 'node:worker_threads'
 
 if (isMainThread) {
-  const threads = process.argv[2]
-  const prefix = process.argv[3]
-  const input = process.argv.slice(4).join('')
+  // Observação quando usar o asterisco, precis utilizar esses argumentos abaixo
+  const threads = isFinite(process.argv[2]) ? process.argv[2] : cpus().length
+  const prefix = process.argv[7] // Sem o asterisco coloque: process.argv[3]
+  const input = process.argv.slice(8).join('') // Sem o asterisco coloque: process.argv.slice(4).join('')
 
-  console.log(` threads: ${threads} / prefix: ${prefix} / input: ${input}`)
+  console.log(`threads: ${threads} | prefix: ${prefix} | input: ${input}`)
 
   const workers = []
   for (let i = 0; i < threads; i++) {
     const worker = new Worker('./index.js', { workerData: { prefix, input } })
-    worker.on('message', () => {
+    worker.on('message', (message) => {
       workers
         .filter((otherWorker) => otherWorker !== worker)
         .forEach((otherWorker) => otherWorker.terminate())
+      console.log(`thread: ${i} | ${message}`)
     })
     workers.push(worker)
   }
@@ -32,7 +35,6 @@ if (isMainThread) {
   do {
     nonce = `${Math.random().toFixed(18).substring(2)}${Math.random().toFixed(18).substring(2)}`
     hash = createHash('sha256').update(`${input}${nonce}`).digest('hex')
-    console.log(`input: ${input} / nonce: ${nonce} / hash: ${hash}`)
   } while (!hash.startsWith(prefix))
-  parentPort.postMessage('END')
+  parentPort.postMessage(`input: ${input} | nonce: ${nonce} | hash: ${hash}`)
 }
